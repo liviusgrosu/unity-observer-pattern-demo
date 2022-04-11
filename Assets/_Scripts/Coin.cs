28b8218f3080a99b918f4e4e319ab74327d2f2f7
liviusgrosu <livius.grosu@ryerson.ca>
2022-04-11 19:57:12 -0400

    Implemented trigger check when player gets close by

--- Assets/_Scripts/Coin.cs
+++ Assets/_Scripts/Coin.cs
@@ -10,25 +10,58 @@ public class Coin : MonoBehaviour
 
     public bool floatUpAndDown;
     Vector3 pointA, pointB;
+    public float heightOffset;
+
+    private bool quitting;
 
     public static event Action CoinCollected;
 
     private void Start() {
-        pointA = transform.position;
-        pointB = new Vector3(transform.position.x, transform.position.y + UnityEngine.Random.Range(0.1f, 0.3f), transform.position.z);
+        Vector3 heightOffsetVector = new Vector3(0f, heightOffset, 0f);
+        
+        pointA = transform.position + heightOffsetVector;
+        pointB = new Vector3(transform.position.x, transform.position.y + UnityEngine.Random.Range(0.1f, 0.3f), transform.position.z) + heightOffsetVector;
     }
 
     private void FixedUpdate() {
         //Spin the object depending on what axis the user requested in edit mode
-        if (x) transform.Rotate(speed, 0, 0);
-        if (y) transform.Rotate(0, speed, 0);
-        if (z) transform.Rotate(0, 0, speed);
+        if (x) 
+        {
+            transform.Rotate(speed, 0, 0);
+        }
+        if (y)
+        {
+            transform.Rotate(0, speed, 0);
+        } 
+        if (z)
+        {
+            transform.Rotate(0, 0, speed);
+        }
 
-        if(floatUpAndDown) transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+        if(floatUpAndDown) 
+        {
+            transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+        }
     }
 
-    private void OnDisable()
+    private void OnTriggerEnter(Collider other)
     {
-        // CoinCollected?.Invoke();
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        } 
+    }
+
+    private void OnDestroy()
+    {
+        if (!quitting)
+        {
+            CoinCollected?.Invoke();
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        quitting = true;
     }
 }
